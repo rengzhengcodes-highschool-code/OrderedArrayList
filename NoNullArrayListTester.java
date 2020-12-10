eff7b4456d45f8d01d82c8b71e3b3ff44452dd2d
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-10 18:37:52 -0500

    Implemented add(index, value) tester which is like the add(value) tester but add(value) is replaced with (add, index value).

--- NoNullArrayListTester.java
+++ NoNullArrayListTester.java
@@ -8,6 +8,7 @@ public class NoNullArrayListTester {
 
 		failure = constructorTester() || failure;
 		failure = addTester(1000) || failure;
+		failure = addAtIndexTester(1000) || failure;
 
 		TesterMethods.overall(failure);
 	}
@@ -89,6 +90,59 @@ public class NoNullArrayListTester {
 
 
 		TesterMethods.methodMessage("addTester", fail);
+		return fail;
+	}
+
+	public static boolean addAtIndexTester(int tests) {
+		TesterMethods.tester("addAtIndexTester");
+		boolean fail = false;
+		NoNullArrayList<Integer> noNulls = new NoNullArrayList<Integer>(tests);
+
+		int nulls = 0;
+		ArrayList<Integer> all = new ArrayList<Integer>(tests);
+
+		for (int test = 0; test < tests; test++) {
+			if (TesterMethods.randInt(10) == 0) {
+				nulls++;
+				all.add(0, null);
+				try {
+					noNulls.add(0, null);
+					fail = true;
+					TesterMethods.errorMessage("Should not be able to insert null.");
+				} catch (IllegalArgumentException e) {
+					//TesterMethods.passMessage(test);
+				}
+			} else {
+				Integer randval = TesterMethods.randInt(-10, 10);
+				all.add(0, randval);
+				noNulls.add(0,randval);
+				//TesterMethods.passMessage(test);
+			}
+		}
+
+		int nullOffset = 0;
+
+		for (int index = 0; index < all.size(); index++) {
+			if (all.get(index) == null) {
+				nullOffset += 1;
+			} else if (all.get(index) == noNulls.get(index - nullOffset)) {
+				//TesterMethods.passMessage(index);
+			} else {
+				fail = true;
+				TesterMethods.errorMessage(Integer.toString(index), Integer.toString(all.get(index)), Integer.toString(noNulls.get(index - nullOffset)));
+			}
+		}
+
+		if ((tests - nulls) == noNulls.size()) {
+			//TesterMethods.passMessage("Amount of elements");
+		} else {
+			fail  = true;
+			TesterMethods.errorMessage("Amount of elements does not line up",  Integer.toString(tests - nulls), Integer.toString(noNulls.size()));
+		}
+
+
+
+		TesterMethods.methodMessage("addAtIndexTester", fail);
 		return fail;
 	}
 
