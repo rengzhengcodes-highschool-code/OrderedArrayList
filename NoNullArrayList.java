6a0098f1d5649ac483d44af95e17dd7c9aec5472
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-10 12:59:47 -0500

    Fixed issue with overriding with incorrect return type on add(T value).

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T> {
 
-	public void add(T value) {
+	public boolean add(T value) {
 		isNull(value);
-		super.add(value);
+		return super.add(value);
 	}
-	
+
 	private static void isNull(Object value) {
 		if (value == null) {
 			throw new IllegalArgumentException("Null values are not valid input.");
