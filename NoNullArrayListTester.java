3ac7ca6fae2034781c9a040f3fcc9291e91f4174
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-10 14:05:49 -0500

    Implemented test for constructors.

--- NoNullArrayListTester.java
+++ NoNullArrayListTester.java
@@ -3,12 +3,36 @@ public class NoNullArrayListTester {
 	public static void main(String[] args) {
 		boolean failure = false;
 
-		System.out.println("\n ~~~ Overall Result ~~~");
-		if (failure) {
-			System.out.println("Coal for you!");
-		} else {
-			System.out.println("Happy Holidays!");
+		failure = constructorTester() || failure;
+
+		TesterMethods.overall(failure);
+	}
+
+	public static boolean constructorTester() {
+		TesterMethods.tester("constructorTester");
+		boolean fail = false;
+
+		try {
+			NoNullArrayList<Object> def = new NoNullArrayList<Object>();
+			TesterMethods.passMessage("default tester");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail = true;
+			TesterMethods.errorMessage("default tester");
 		}
+
+		try {
+			int randSize = TesterMethods.randInt(8);
+			NoNullArrayList<Object> rand = new NoNullArrayList<Object>(randSize);
+			TesterMethods.passMessage("randSize tester");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail = true;
+			TesterMethods.errorMessage("randSize tester");
+		}
+
+		TesterMethods.methodMessage("constructorTester", fail);
+		return fail;
 	}
 
 }
