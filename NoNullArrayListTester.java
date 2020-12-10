d7af69a18e846a5a360ac95593624d3f468c39da
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-10 13:48:17 -0500

    NoNullArrayList tester file created (no tests created).

---
+++ NoNullArrayListTester.java
@@ -0,0 +1,14 @@
+public class NoNullArrayListTester {
+
+	public static void main(String[] args) {
+		boolean failure = false;
+
+		System.out.println("\n ~~~ Overall Result ~~~");
+		if (failure) {
+			System.out.println("Coal for you!");
+		} else {
+			System.out.println("Happy Holidays!");
+		}
+	}
+
+}
