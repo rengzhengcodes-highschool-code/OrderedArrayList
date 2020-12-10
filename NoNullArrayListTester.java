bf7eb63257b0e254cc044cbc0ac41c6dd74d8ceb
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-10 18:34:12 -0500

    Comprehensive test for add(value).

--- NoNullArrayListTester.java
+++ NoNullArrayListTester.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NoNullArrayListTester {
 
 	public static void main(String[] args) {
 		boolean failure = false;
 
 		failure = constructorTester() || failure;
+		failure = addTester(1000) || failure;
 
 		TesterMethods.overall(failure);
 	}
@@ -14,7 +18,7 @@ public class NoNullArrayListTester {
 
 		try {
 			NoNullArrayList<Object> def = new NoNullArrayList<Object>();
-			TesterMethods.passMessage("default tester");
+			//TesterMethods.passMessage("default tester");
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail = true;
@@ -24,7 +28,7 @@ public class NoNullArrayListTester {
 		try {
 			int randSize = TesterMethods.randInt(8);
 			NoNullArrayList<Object> rand = new NoNullArrayList<Object>(randSize);
-			TesterMethods.passMessage("randSize tester");
+			//TesterMethods.passMessage("randSize tester");
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail = true;
@@ -32,6 +36,59 @@ public class NoNullArrayListTester {
 		}
 
 		TesterMethods.methodMessage("constructorTester", fail);
+		return fail;
+	}
+
+	public static boolean addTester(int tests) {
+		TesterMethods.tester("addTester");
+		boolean fail = false;
+		NoNullArrayList<Integer> noNulls = new NoNullArrayList<Integer>(tests);
+
+		int nulls = 0;
+		ArrayList<Integer> all = new ArrayList<Integer>(tests);
+
+		for (int test = 0; test < tests; test++) {
+			if (TesterMethods.randInt(10) == 0) {
+				nulls++;
+				all.add(null);
+				try {
+					noNulls.add(null);
+					fail = true;
+					TesterMethods.errorMessage("Should not be able to insert null.");
+				} catch (IllegalArgumentException e) {
+					//TesterMethods.passMessage(test);
+				}
+			} else {
+				Integer randval = TesterMethods.randInt(-10, 10);
+				all.add(randval);
+				fail = fail || !noNulls.add(randval);
+				//TesterMethods.passMessage(test);
+			}
+		}
+
+		int nullOffset = 0;
+
+		for (int index = 0; index < all.size(); index++) {
+			if (all.get(index) == null) {
+				nullOffset += 1;
+			} else if (all.get(index) == noNulls.get(index - nullOffset)) {
+				//TesterMethods.passMessage(index);
+			} else {
+				fail = true;
+				TesterMethods.errorMessage(Integer.toString(index), Integer.toString(all.get(index)), Integer.toString(noNulls.get(index - nullOffset)));
+			}
+		}
+
+		if ((tests - nulls) == noNulls.size()) {
+			//TesterMethods.passMessage("Amount of elements");
+		} else {
+			fail  = true;
+			TesterMethods.errorMessage("Amount of elements does not line up",  Integer.toString(tests - nulls), Integer.toString(noNulls.size()));
+		}
+
+
+
+		TesterMethods.methodMessage("addTester", fail);
 		return fail;
 	}
 
