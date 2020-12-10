7058d1dd0b435d7f9b15cfe441e05065076b0aea
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-10 12:49:39 -0500

    Added isNull method which checks if input == null and throws an IllegalArgumentException.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T> {
-	private ArrayList<T> values;
 
-	public NoNullArrayList()  {
-		this.values = new ArrayList<T>();
+	public void add(T value) {
+		isNull(value);
+		super.add(value);
+	}
+	
+	private static void isNull(Object value) {
+		if (value == null) {
+			throw new IllegalArgumentException("Null values are not valid input.");
+		}
 	}
 
-	
 }
