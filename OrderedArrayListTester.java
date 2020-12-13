9d65c5a9e1d00c454f58b1721320aae2bf101a95
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-12 21:46:45 -0500

    Added tester for nullValue checks on all overridden functions.

--- OrderedArrayListTester.java
+++ OrderedArrayListTester.java
@@ -4,6 +4,7 @@ public class OrderedArrayListTester {
 		boolean failure = false;
 
 		failure = constructorTester() || failure;
+		failure = nullTester() || failure;
 
 		TesterMethods.overall(failure);
 	}
@@ -39,8 +40,50 @@ public class OrderedArrayListTester {
 			fail = true;
 			TesterMethods.errorMessage("Improper class extension.");
 		}
-		
+
 		TesterMethods.methodMessage("constructorTester", fail);
+		return fail;
+	}
+
+	public static boolean nullTester() {
+		TesterMethods.tester("nullTester");
+		boolean fail = false;
+
+		OrderedArrayList<Integer> test = new OrderedArrayList<Integer>();
+
+		for (int n = 0; n < 10; n++) {
+			test.add(n);
+		}
+
+		try {
+			test.add(null);
+			fail = true;
+			TesterMethods.errorMessage("add(value) added a null");
+		} catch (IllegalArgumentException e) {
+			//TesterMethods.passMessage("add(value) didn't add a null");
+		}
+
+		try {
+			for (int i = 0; i < 100; i++) {
+				test.add(TesterMethods.randInt(10), null);
+			}
+			fail = true;
+			TesterMethods.errorMessage("add(index, value) added a null");
+		} catch (IllegalArgumentException e) {
+			//TesterMethods.passMessage("add(index, value) didn't add a null");
+		}
+
+		try {
+			for (int i = 0; i < 100; i++) {
+				test.set(TesterMethods.randInt(10), null);
+			}
+			fail = true;
+			TesterMethods.errorMessage("set(index, value) added a null");
+		} catch (IllegalArgumentException e) {
+			//TesterMethods.passMessage("set(index, value) didn't add a null");
+		}
+
+		TesterMethods.methodMessage("nullTester", fail);
 		return fail;
 	}
 }
